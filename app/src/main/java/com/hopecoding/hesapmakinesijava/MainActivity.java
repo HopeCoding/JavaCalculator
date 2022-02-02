b9a12ce05847450cfe6754b2052ebc01d918befd
HopeCoding <ucan.aldirmaz@gmail.com>
2022-02-02 21:16:34 +0300

    Değer girmediğinde app çökmesi düzeltildi.

--- app/src/main/java/com/hopecoding/hesapmakinesijava/MainActivity.java
+++ app/src/main/java/com/hopecoding/hesapmakinesijava/MainActivity.java
@@ -13,58 +13,78 @@ import java.text.MessageFormat;
 public class MainActivity extends AppCompatActivity {
     public static double first;
     public static double second;
-    public static double total=0;
+    public static double total = 0;
 
     EditText firstText;
     EditText secondText;
     TextView resultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         firstText=findViewById(R.id.firstText);
-         secondText=findViewById(R.id.secondText);
-         resultText=findViewById(R.id.resultText);
-
+        firstText = findViewById(R.id.firstText);
+        secondText = findViewById(R.id.secondText);
+        resultText = findViewById(R.id.resultText);
 
 
     }
 
 
-    public void topla(View view){
-        first=Double.parseDouble(firstText.getText().toString());
-        second=Double.parseDouble(secondText.getText().toString());
-        total=first+second;
+    public void topla(View view) {
+        if (firstText.getText().toString().matches("") || secondText.getText().toString().matches("")){
+            resultText.setText("Don't forget to enter a number");
+        }else{
+            first = Double.parseDouble(firstText.getText().toString());
+            second = Double.parseDouble(secondText.getText().toString());
+            total = first + second;
 
-        resultText.setText(MessageFormat.format("Sonuç:{0}", total));
+            resultText.setText(MessageFormat.format("Sonuç:{0}", total));
 
-    }
+        }
 
-    public void cikar(View view){
-        first=Double.parseDouble(firstText.getText().toString());
-        second=Double.parseDouble(secondText.getText().toString());
-        total=first-second;
-
-        resultText.setText(MessageFormat.format("Sonuç:{0}", total));
 
     }
 
-    public void carp(View view){
-        first=Double.parseDouble(firstText.getText().toString());
-        second=Double.parseDouble(secondText.getText().toString());
-        total=first*second;
+    public void cikar(View view) {
+        if (firstText.getText().toString().matches("") || secondText.getText().toString().matches("")){
+            resultText.setText("Don't forget to enter a number");
+        }else{
+            first = Double.parseDouble(firstText.getText().toString());
+            second = Double.parseDouble(secondText.getText().toString());
+            total = first - second;
 
-        resultText.setText(MessageFormat.format("Sonuç:{0}", total));
+            resultText.setText(MessageFormat.format("Sonuç:{0}", total));
+        }
+
 
     }
 
-    public void bolme(View view){
-         first=Double.parseDouble(firstText.getText().toString());
-         second=Double.parseDouble(secondText.getText().toString());
-         total=first/second;
+    public void carp(View view) {
+        if (firstText.getText().toString().matches("") || secondText.getText().toString().matches("")){
+            resultText.setText("Don't forget to enter a number");
+        }else{
+            first = Double.parseDouble(firstText.getText().toString());
+            second = Double.parseDouble(secondText.getText().toString());
+            total = first * second;
 
-        resultText.setText(MessageFormat.format("Sonuç:{0}", total));
+            resultText.setText(MessageFormat.format("Sonuç:{0}", total));
+        }
+
+
+    }
+
+    public void bolme(View view) {
+        if (firstText.getText().toString().matches("") || secondText.getText().toString().matches("")){
+            resultText.setText("Don't forget to enter a number");
+        }else{
+            first = Double.parseDouble(firstText.getText().toString());
+            second = Double.parseDouble(secondText.getText().toString());
+            total = first / second;
+
+            resultText.setText(MessageFormat.format("Sonuç:{0}", total));
+        }
 
     }
 }
